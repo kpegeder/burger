ca95e99eb2402d596057c5647948c20be515b84d
kpegeder <k.pegeder@gmail.com>
2020-10-07 16:34:39 -0700

    intial commit

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Delete and Create an empolyee database
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+-- Use Employee db
+USE burgers_db;
+
+-- Create tables
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(30) NOT NULL,
+  devoured BOOLEAN,
+  PRIMARY KEY (id)
+);
