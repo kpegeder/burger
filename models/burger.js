8b980e9ca8331b412c58184df67e0810c1f1d829
kpegeder <k.pegeder@gmail.com>
2020-10-08 16:16:46 -0700

    create a burger orm

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+// Import the ORM to create functions that will interact with the DATABASE
+const orm = require("../config/orm.js");
+
+const burger = {
+  all: function (cb) {
+    orm.all("burgers", function (res) {
+      cb(res);
+    });
+  },
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  update: function (objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
+};
+
+// Export the database funtions for the controller (burgers_controller.js)
+module.exports = burger;
