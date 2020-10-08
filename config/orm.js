675ece05a23592861a6826df45b2bea449dd4d1d
kpegeder <k.pegeder@gmail.com>
2020-10-08 16:16:13 -0700

    set up the basic format for the orm

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,43 @@
 // Import MySQL connection
 const connection = require("../config/connection.js");
 
+// Helper function for MySQL syntax
+function printQuestionMarks(num) {
+  let arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to MySQL syntax
+function objToSql(ob) {
+  let arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (let key in ob) {
+    let value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
 // Object for all our SQL statment functions
 const orm = {
-  select: function (table, cb) {
+  all: function (table, cb) {
     const queryString = "SELECT * FROM " + table + ";";
     connection.query(queryString, function (err, result) {
       if (err) {
@@ -12,14 +46,14 @@ const orm = {
       cb(result);
     });
   },
-  insert: function (table, cols, vals, cb) {
+  create: function (table, cols, vals, cb) {
     let queryString = "INSERT INTO " + table;
 
     queryString += " (";
     queryString += cols.toString();
     queryString += ") ";
     queryString += "VALUES (";
-    queryString += "'" + vals[0] + "'" + "," + vals[1];
+    queryString += printQuestionMarks(vals.length);
     queryString += ") ";
 
     console.log(queryString);
@@ -53,25 +87,3 @@ const orm = {
 
 // Export the orm object for the model
 module.exports = orm;
-
-function objToSql(ob) {
-  var arr = [];
-
-  // loop through the keys and push the key/value as a string int arr
-  for (var key in ob) {
-    var value = ob[key];
-    // check to skip hidden properties
-    if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = "'" + value + "'";
-      }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
-      arr.push(key + "=" + value);
-    }
-  }
-
-  // translate array of strings to a single comma-separated string
-  return arr.toString();
-}
