e2683c34f829915befaebd24096ffea50c0b737c
kpegeder <k.pegeder@gmail.com>
2020-10-09 21:06:59 -0700

    create and complete js for frontend

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,39 @@
+$(function () {
+  $(".change-devour").on("click", function (event) {
+    let id = $(this).data("id");
+    let newDevour = $(this).data("newdevour");
+
+    console.log(id);
+    let newBurgerState = {
+      devoured: newDevour,
+    };
+
+    // Send the Put request
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newBurgerState,
+    }).then(function () {
+      console.log("changed burger to ", newBurgerState);
+      // Reload the page to update list
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", function (event) {
+    // Make sure to preventDefault to a submit event
+    event.preventDefault();
+
+    let newBurger = {
+      name: $("#burg").val().trim(),
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("Created a new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+});
