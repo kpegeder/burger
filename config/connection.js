b21d28f7297e2aa79cfdbafacdbd1593ed1c3bf3
kpegeder <k.pegeder@gmail.com>
2020-10-11 17:47:35 -0700

    update connection to database

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "homework",
-  password: "Hello123",
-  database: "burgers_db",
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "homework",
+    password: "Hello123",
+    database: "burgers_db",
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
